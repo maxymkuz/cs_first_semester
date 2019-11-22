2264cadbacf16213a1cfaab445f210e384b90543
max <maxymkuz@gmail.com>
2019-11-22 14:39:42 +0200

    nazarii

--- lection8_dict_set/labka3.py
+++ lection8_dict_set/labka3.py
@@ -1,11 +1,7 @@
 def dict_reader_tuple(file_dict):
     with open(file_dict, 'r', encoding='utf-8') as f:
-        res = []
-        for line in f:
-            line = line.strip().split()
-            tpl = (line[0], line[1], line[2:])
-            res.append((line[0], line[1], line[2:]))
-    return res
+        ans = [(line.strip().split()[0], line.strip().split()[1], line.strip().split()[2:]) for line in f]
+    return ans
 
 
 def dict_reader_dict(file_dict):
@@ -13,14 +9,23 @@ def dict_reader_dict(file_dict):
     with open(file_dict, 'r', encoding='utf-8') as f:
         for line in f:
             line = line.strip().split()
-            print(line[0], line)
-            dictionary
-            # dictionary[line[0]] = set(dictionary[line[0]])
-    print(dictionary)
+            if line[0] not in dictionary:
+                x = set()
+                x.add(tuple(line[2:]))
+                dictionary[line[0]] = x
+            else:
+                temp = dictionary.get(line[0])
+                temp.add(tuple(line[2:]))
+    return dictionary
 
 
-dict_reader_dict('cmudict2.txt')
+# print(dict_reader_dict('cmudict2.txt'))
 
 
 def dict_invert(dct):
-    pass
+    """
+    >>> dict_invert({'WATER':{('W','A','T','E','R')}})
+    {1: {('WATER', ('W','A','T','E','R'))}}
+    """
+    if isinstance(dct, dict):
+        
