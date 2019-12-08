577f824b526094dc2895e985f1cf518887ff67d5
max <maxymkuz@gmail.com>
2019-12-08 19:02:43 +0200

    Labka lection 13

---
+++ lection_12_Clion/fourth.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(){
+
+    double num;
+    int res = 0;
+    while(1 > 0) {
+        printf("Enter num");
+        scanf("%lf", &num);
+        num *= 100;
+
+        if(num == (int)num && num >= 0){
+            num = (int)num;
+            break;
+        }
+    }
+
+
+    int coins[4] = {25,10,5,1};
+    for (int i = 0; i < 4; ++i) {
+        while(num >= coins[i]){
+            num -= coins[i];
+            res++;
+        }
+    }
+    printf("Result: %d", res);
+    return 0;
+}
