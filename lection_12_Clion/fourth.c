dc4b0343467c57eccab369182f5008bf89fe3d3c
max <maxymkuz@gmail.com>
2019-12-08 20:03:34 +0200

    ajkghfhkasdjhkg

--- lection_12_Clion/fourth.c
+++ lection_12_Clion/fourth.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-
 int main(){
-
     double num;
     int res = 0;
+
     while(1 > 0) {
         printf("Enter num");
         scanf("%lf", &num);
         num *= 100;
-
         if(num == (int)num && num >= 0){
             num = (int)num;
             break;
         }
     }
-
 
     int coins[4] = {25,10,5,1};
     for (int i = 0; i < 4; ++i) {
--- lection_12_Clion/mario.c
+++ lection_12_Clion/mario.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     int height;
-    scanf("%d", &height);
+    char* str[10];
+
+
+    while(1 > 0){
+        printf("enter height: ");
+        char input[100];
+        scanf("%s", input);
+        int length = strlen(input);
+
+        if(length > 2 || length == 0 || input[0] > '9' || input[0] < '0'){
+            continue;
+        }
+        if(length == 2){
+            if(input[1] > '9' || input[1] < '0')
+                continue;
+        }
+        sscanf(input, "%d", &height);
+
+        if(height<=24)
+            break;
+
+        }
+
     for (int i = 2; i < height + 2; i++){
         for (int j = 0; j < height-i + 1; j++)
         {
